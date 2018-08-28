43056c12af3ab0ccce88a890eafcecdffd4e8d46
LXS <18614036706@163.com>
2018-08-28 22:18:54 +0800

    a

--- modelsim/work/traffic_led/_primary.vhd
+++ modelsim/work/traffic_led/_primary.vhd
@@ -2,8 +2,7 @@ library verilog;
 use verilog.vl_types.all;
 entity traffic_led is
     generic(
-        TIME_1S         : integer := 5000000;
-        TIME_4S         : integer := 4
+        TIME_1s         : integer := 5000000
     );
     port(
         clk             : in     vl_logic;
@@ -14,6 +13,5 @@ entity traffic_led is
         led_north       : out    vl_logic_vector(2 downto 0)
     );
     attribute mti_svvh_generic_type : integer;
-    attribute mti_svvh_generic_type of TIME_1S : constant is 1;
-    attribute mti_svvh_generic_type of TIME_4S : constant is 1;
+    attribute mti_svvh_generic_type of TIME_1s : constant is 1;
 end traffic_led;
